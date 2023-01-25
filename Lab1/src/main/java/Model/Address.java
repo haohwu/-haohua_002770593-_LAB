594e5a4389845becfbc90ae61ed17070b3d6724f
haohwu <wu.haoh@northeastern.edu>
2023-01-24 22:48:00 -0500

    update

--- Lab1/src/main/java/Model/Address.java
+++ Lab1/src/main/java/Model/Address.java
@@ -10,15 +10,47 @@ package Model;
  */
 public class Address {
     String street;
+    Integer apt;
+    String city;
+    String state;
+    Integer zipcode;
     
     public Address(){
         this.street = "";
+        this.apt = "";
+        this.city = "";
+        this.state = "";
+        this.zipcode = "";
     }
     public String getStreet(){
         return street;
     }
     public void setStreet(String street) {
-        this.street = street;
-        
+        this.street = street;       
     }
+    public Integer getApt(){
+        return apt;
+    }
+    public void setApt(Integer apt){
+        this.apt = apt;
+    }
+    public String getCity(){
+        return city;
+    }
+    public void setCity(String city){
+        this.city = city;
+    }
+    public String getState(){
+        return state;
+    }
+    public void setState(String state){
+        this.state = state;
+    }
+    public Interger setZipCode(){
+        return zipcode;
+    }
+    public void setZipCode(Integer zipcode){
+        this.zipcode = zipcode;
+    }
+    
 }
